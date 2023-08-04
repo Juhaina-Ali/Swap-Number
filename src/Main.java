44d985213fb079ac143f61ee7c022ee2dbadbdb9
Juhaina-Ali <133102246+Juhaina-Ali@users.noreply.github.com>
2023-08-04 18:47:25 +0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        double x = 2.0, y = 7.0, z;
+        z = 2.0;
+        x = 7.0;
+        y = z;
+        System.out.println("x = "+x);
+        System.out.println("y = "+y);
+
+    }
+}
